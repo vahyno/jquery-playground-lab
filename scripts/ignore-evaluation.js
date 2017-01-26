69e818bbe3ad447480352180da8a72324e0be8fe
nathanallen <thenathanator@gmail.com>
2017-01-25 21:46:58 -0800

    place warnings after tabular output

--- scripts/ignore-evaluation.js
+++ scripts/ignore-evaluation.js
@@ -6,23 +6,32 @@ if (!window.problem_set) {
   alert("You broke the lab!")
 }
 
-var output = [];
+var results = [],
+    warnings = [];
 
 for (var k in problem_set) {
   let problem = problem_set[k]
 
-  if (!problem instanceof Question) {
-    console.warn(
-      `Woops! Looks like you overwrote the question instead of answering it!
+  if (problem instanceof Question) {
+
+    results.push({
+      prompt: k.replace(/_/g, " "),
+      "is my answer correct?": problem.check_answer() ? "yes" : "no"
+    })
+
+  } else {
+
+    warnings.push(
+      `Woops! Looks like you overwrote a question instead of answering it!
       Fix your code to say: \`problem_set.${k} = \"YOUR ANSWER HERE\";\``
     );
-    continue;
+
   }
 
-  output.push({
-    prompt: k.replace(/_/g, " "),
-    "is my answer correct?": problem.check_answer() ? "yes" : "no"
-  })
 }
 
-console.table(output);
+console.table(results);
+
+warnings.forEach(function(w){
+  console.warn(w);
+});
