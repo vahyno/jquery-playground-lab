749f122bea17f833aaa73c9aa9a7d6d4be374295
nathanallen <thenathanator@gmail.com>
2017-01-25 20:11:35 -0800

    solutions

---
+++ scripts/solution.js
@@ -0,0 +1,46 @@
+// SPOILERS BELOW
+
+// SPOILERS BELOW
+
+
+// SPOILERS BELOW
+
+
+
+
+// SPOILERS BELOW
+
+
+
+
+
+
+
+
+// SPOILERS BELOW
+
+
+problem_set.how_many_h1_tags_are_on_the_page.answer = $("h1").length;
+
+problem_set.how_many_p_tags_are_on_the_page.answer = $("p").length;
+
+problem_set.grab_all_the_buttons.answer = $("button");
+
+problem_set.grab_the_red_button.answer = $("button.alert-danger");
+
+problem_set.grab_the_blue_button.answer = $("button.alert-info")
+
+problem_set.what_time_is_it.answer = $("h1 span").text();
+
+problem_set.how_tall_is_the_image_in_pixels.answer = $("img").height();
+
+problem_set.how_wide_is_the_image_in_pixels.answer = $("img").width();
+
+problem_set.what_is_the_image_url.answer = $("img").attr("src");
+
+problem_set.what_does_the_question_field_say.answer = $("input#question").val();
+
+var left = parseInt( $(".add-me").eq(0).val() );
+var right = parseInt( $(".add-me").eq(1).val() );
+problem_set.what_is_the_sum_of_the_two_numbers.answer = left + right;
+
