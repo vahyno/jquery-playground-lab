4915b5426a17abcaa613655862140f37f2e811bd
vahyno <vahyno@gmail.com>
2018-03-17 18:58:20 -0700

    part of the solution done

--- scripts/challenge.js
+++ scripts/challenge.js
@@ -2,26 +2,26 @@
 
 problem_set.how_many_h1_tags_are_on_the_page.answer = $("h1").length;
 
-problem_set.how_many_p_tags_are_on_the_page.answer = "YOUR ANSWER HERE";
+problem_set.how_many_p_tags_are_on_the_page.answer = $("p").length;;
 
-problem_set.grab_all_the_colorful_message_elements.answer = "YOUR ANSWER HERE";
+problem_set.grab_all_the_colorful_message_elements.answer = $(".alert");
 
-problem_set.grab_the_red_message_element.answer = "YOUR ANSWER HERE";
+problem_set.grab_the_red_message_element.answer = $(".alert-danger");
 
-problem_set.grab_the_blue_message_element.answer = "YOUR ANSWER HERE";
+problem_set.grab_the_blue_message_element.answer = $(".alert-info");
 
-problem_set.what_time_is_on_the_page.answer = "YOUR ANSWER HERE";
+problem_set.what_time_is_on_the_page.answer = $(".time").text();
 
-problem_set.what_does_the_question_field_say.answer = "YOUR ANSWER HERE";
+problem_set.what_does_the_question_field_say.answer = $('#question').val();
 
-problem_set.replace_the_question_field_with_the_word_yes; // MODIFY THE DOM DIRECTLY!
+problem_set.replace_the_question_field_with_the_word_yes; $('#question').val('yes');// MODIFY THE DOM DIRECTLY!
 // your solution code here
 
-problem_set.how_tall_is_the_image_in_pixels.answer = "YOUR ANSWER HERE";
+problem_set.how_tall_is_the_image_in_pixels.answer = $(".img-thumbnail").height();
 
-problem_set.how_wide_is_the_image_in_pixels.answer = "YOUR ANSWER HERE";
+problem_set.how_wide_is_the_image_in_pixels.answer = $(".img-thumbnail").width();
 
-problem_set.what_is_the_image_url.answer = "YOUR ANSWER HERE";
+problem_set.what_is_the_image_url.answer = $("img").attr('src');
 
 problem_set.what_is_the_sum_of_the_two_numbers.answer = "YOUR ANSWER HERE";
 
